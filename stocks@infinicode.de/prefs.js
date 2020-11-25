950b4c158e265f95075df54e7a1dbe2f0428bbbf
Florijan Hamzic <fh@infinicode.de>
2020-11-25 23:44:47 +0100

    imrpove settings, add ticker interval

--- stocks@infinicode.de/prefs.js
+++ stocks@infinicode.de/prefs.js
@@ -1,21 +1,20 @@
-const Gtk = imports.gi.Gtk
-const GObject = imports.gi.GObject
-const Gettext = imports.gettext.domain('stocks@infinicode.de')
-const _ = Gettext.gettext
+const { GLib, GObject, Gtk } = imports.gi
 
 const Mainloop = imports.mainloop
+
 const ExtensionUtils = imports.misc.extensionUtils
 const Me = ExtensionUtils.getCurrentExtension()
-const Convenience = Me.imports.convenience
+const Settings = Me.imports.helpers.settings
+
+const { decodeBase64JsonOrDefault, isNullOrEmpty } = Me.imports.helpers.data
+const { initTranslations, Translations } = Me.imports.helpers.translations
 
 const EXTENSIONDIR = Me.dir.get_path()
 
-var STOCKS_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.stocks'
-var STOCKS_POSITION_IN_PANEL_KEY = 'position-in-panel'
-var STOCKS_SYMBOL_PAIRS = 'symbol-pairs'
-var STOCKS_SYMBOL_CURRENT_QUOTES = 'symbol-current-quotes'
+const POSITION_IN_PANEL_KEY = 'position-in-panel'
+const STOCKS_SYMBOL_PAIRS = 'symbol-pairs'
+const STOCKS_TICKER_INTERVAL = 'ticker-interval'
 
-let currentSymbolData = null
 let inRealize = false
 let defaultSize = [-1, -1]
 
@@ -30,7 +29,8 @@ var PrefsWidget = GObject.registerClass({
     if (!this.Settings) {
       this.loadConfig()
     }
-    return this.Settings.get_enum(STOCKS_POSITION_IN_PANEL_KEY)
+
+    return this.Settings.get_enum(POSITION_IN_PANEL_KEY)
   }
 
   set position_in_panel (v) {
@@ -38,39 +38,69 @@ var PrefsWidget = GObject.registerClass({
       this.loadConfig()
     }
 
-    this.Settings.set_enum(STOCKS_POSITION_IN_PANEL_KEY, v)
+    this.Settings.set_enum(POSITION_IN_PANEL_KEY, v)
   }
 
   get symbolPairs () {
     if (!this.Settings) {
       this.loadConfig()
     }
-    return this.Settings.get_string(STOCKS_SYMBOL_PAIRS)
+
+    const rawString = this.Settings.get_string(STOCKS_SYMBOL_PAIRS)
+
+    /****
+     * For backwards compatiblity intercept here if it contains certain string -&&- && -§§-
+     * if we found old format convert to new format and save
+     */
+    if (rawString.includes('-&&-') && rawString.includes('-§§-')) {
+      try {
+        let newData = Settings.convertOldSettingsFormat(rawString)
+
+        if (!newData) {
+          newData = Settings.DEFAULT_SYMBOL_DATA
+        }
+
+        this.Settings.set_string(STOCKS_SYMBOL_PAIRS, GLib.base64_encode(JSON.stringify(newData)))
+
+        return newData
+      } catch (e) {
+        log(`failed to convert old data ${e}`)
+        return Settings.DEFAULT_SYMBOL_DATA
+      }
+    }
+
+    const stockItems = decodeBase64JsonOrDefault(rawString, Settings.DEFAULT_SYMBOL_DATA)
+
+    if (isNullOrEmpty(stockItems)) {
+      this.Settings.set_string(STOCKS_SYMBOL_PAIRS, GLib.base64_encode(JSON.stringify(Settings.DEFAULT_SYMBOL_DATA)))
+      return Settings.DEFAULT_SYMBOL_DATA
+    }
+
+    return stockItems
   }
 
   set symbolPairs (v) {
     if (!this.Settings) {
       this.loadConfig()
     }
-    this.Settings.set_string(STOCKS_SYMBOL_PAIRS, v)
+
+    this.Settings.set_string(STOCKS_SYMBOL_PAIRS, GLib.base64_encode(JSON.stringify(v)))
   }
 
-  get current_quotes () {
+  get ticker_interval () {
     if (!this.Settings) {
       this.loadConfig()
     }
-    return this.Settings.get_string(STOCKS_SYMBOL_CURRENT_QUOTES)
+
+    return this.Settings.get_int(STOCKS_TICKER_INTERVAL)
   }
 
-  set current_quotes (v) {
+  set ticker_interval (v) {
     if (!this.Settings) {
       this.loadConfig()
     }
-    return this.Settings.set_string(STOCKS_SYMBOL_CURRENT_QUOTES, v)
-  }
 
-  get splittedSymbolPairs () {
-    return this.symbolPairs.split('-&&-')
+    this.Settings.set_int(STOCKS_TICKER_INTERVAL, v)
   }
 
   _init (params = {}) {
@@ -80,6 +110,7 @@ var PrefsWidget = GObject.registerClass({
     }))
 
     this.configWidgets = []
+    this.componentTimeoutIds = {}
     this.Window = new Gtk.Builder()
 
     this.initWindow()
@@ -106,28 +137,46 @@ var PrefsWidget = GObject.registerClass({
       this.MainWidget.get_toplevel().resize(defaultSize[0], defaultSize[1])
       inRealize = false
     })
+
+    this.treeview.connect('drag-drop', (a, b, c) => {
+      const treeModel = this.treeview.get_model()
+
+      const newStockModels = []
+
+      treeModel.foreach((model, path, iter) => {
+        newStockModels.push({
+          name: model.get_value(iter, 0),
+          symbol: model.get_value(iter, 1),
+          showInTicker: model.get_value(iter, 2)
+        })
+      })
+
+      this.symbolPairs = newStockModels
+    })
   }
 
   initWindow () {
-    currentSymbolData = null
-
     this.Window.add_from_file(EXTENSIONDIR + '/settings.ui')
 
     this.MainWidget = this.Window.get_object('main-widget')
 
     let theObjects = this.Window.get_objects()
     for (let i in theObjects) {
-      let name = theObjects[i].get_name ? theObjects[i].get_name() : 'dummy'
+      const name = theObjects[i].get_name ? theObjects[i].get_name() : 'dummy'
 
       if (this[name] !== undefined) {
-        if (theObjects[i].class_path()[1].indexOf('GtkEntry') != -1) {
+        const classPath = theObjects[i].class_path()[1]
+
+        if (classPath.indexOf('GtkEntry') != -1) {
           this.initEntry(theObjects[i])
-        } else if (theObjects[i].class_path()[1].indexOf('GtkComboBoxText') != -1) {
+        } else if (classPath.indexOf('GtkComboBoxText') != -1) {
           this.initComboBox(theObjects[i])
-        } else if (theObjects[i].class_path()[1].indexOf('GtkSwitch') != -1) {
+        } else if (classPath.indexOf('GtkSwitch') != -1) {
           this.initSwitch(theObjects[i])
-        } else if (theObjects[i].class_path()[1].indexOf('GtkScale') != -1) {
+        } else if (classPath.indexOf('GtkScale') != -1) {
           this.initScale(theObjects[i])
+        } else if (classPath.indexOf('GtkSpinButton') != -1) {
+          this.initSpinner(theObjects[i])
         }
 
         this.configWidgets.push([theObjects[i], name])
@@ -138,19 +187,18 @@ var PrefsWidget = GObject.registerClass({
       this.Window.get_object('version').set_label(Me.metadata.version.toString())
     }
 
-    this.mainWidget = this.Window.get_object('prefs-notebook')
     this.treeview = this.Window.get_object('tree-treeview')
     this.liststore = this.Window.get_object('tree-liststore')
+
     this.createWidget = this.Window.get_object('create-symbol-widget')
     this.newName = this.Window.get_object('new-name')
     this.newSymbol = this.Window.get_object('new-symbol')
-
-    this.editName = this.Window.get_object('edit-name')
-    this.editSymbol = this.Window.get_object('edit-symbol')
+    this.newShowInTicker = this.Window.get_object('new-show-in-ticker')
 
     this.editWidget = this.Window.get_object('edit-symbol-widget')
     this.editName = this.Window.get_object('edit-name')
     this.editSymbol = this.Window.get_object('edit-symbol')
+    this.editShowInTicker = this.Window.get_object('edit-show-in-ticker')
 
     // TreeView / Table Buttons
     this.Window.get_object('tree-toolbutton-add').connect('clicked', () => {
@@ -179,7 +227,7 @@ var PrefsWidget = GObject.registerClass({
    * Load Config Data from file and connect change event
    */
   loadConfig () {
-    this.Settings = Convenience.getSettings(STOCKS_SETTINGS_SCHEMA)
+    this.Settings = Settings.getSettings()
     this.Settings.connect('changed', this.evaluateValues.bind(this))
   }
 
@@ -202,6 +250,32 @@ var PrefsWidget = GObject.registerClass({
       } else {
         theEntry.set_icon_from_icon_name(Gtk.PositionType.LEFT, 'dialog-warning')
       }
+    })
+  }
+
+  /**
+   * initialize entry items (input boxes)
+   * @param theEntry entry element
+   */
+  initSpinner (theEntry) {
+    const name = theEntry.get_name()
+
+    theEntry.set_value(this[name])
+
+    // prevent from continously updating the value
+    delete this.componentTimeoutIds[name]
+
+    theEntry.connect('value-changed', (button) => {
+      const timeoutId = this.componentTimeoutIds[name]
+
+      if (timeoutId) {
+        Mainloop.source_remove(timeoutId)
+      }
+
+      this.componentTimeoutIds[name] = Mainloop.timeout_add(250, () => {
+        this[name] = button.get_value()
+        return false
+      })
     })
   }
 
@@ -251,29 +325,38 @@ var PrefsWidget = GObject.registerClass({
    * Initialize TreeView (Symbol Table)
    */
   initTreeView () {
-    let column = new Gtk.TreeViewColumn()
-    column.set_title(_('Name'))
-    this.treeview.append_column(column)
+    /**** name cell ****/
+    const quoteNameColumn = new Gtk.TreeViewColumn()
+    quoteNameColumn.set_title(Translations.SETTINGS.QUOTE_NAME)
+    this.treeview.append_column(quoteNameColumn)
 
     let renderer = new Gtk.CellRendererText()
-    column.pack_start(renderer, null)
+    quoteNameColumn.pack_start(renderer, null)
 
-    column.set_cell_data_func(renderer, function () {
-      if (arguments && arguments.length) {
-        arguments[1].markup = arguments[2].get_value(arguments[3], 0)
-      }
+    quoteNameColumn.set_cell_data_func(renderer, (tree, cell, model, iter) => {
+      cell.markup = model.get_value(iter, 0)
     })
 
-    column = new Gtk.TreeViewColumn()
-    column.set_title(_('Symbol (Yahoo)'))
-    this.treeview.append_column(column)
+    /**** symbol cell ****/
+    const symbolColumn = new Gtk.TreeViewColumn()
+    symbolColumn.set_title(Translations.SETTINGS.SYMBOL)
+    this.treeview.append_column(symbolColumn)
 
-    column.pack_start(renderer, null)
+    symbolColumn.pack_start(renderer, null)
 
-    column.set_cell_data_func(renderer, function () {
-      if (arguments && arguments.length) {
-        arguments[1].markup = arguments[2].get_value(arguments[3], 1)
-      }
+    symbolColumn.set_cell_data_func(renderer, (tree, cell, model, iter) => {
+      cell.markup = model.get_value(iter, 1)
+    })
+
+    /**** ticker cell ****/
+    const showInTicker = new Gtk.TreeViewColumn()
+    showInTicker.set_title(Translations.SETTINGS.SHOW_IN_TICKER)
+    this.treeview.append_column(showInTicker)
+
+    showInTicker.pack_start(renderer, null)
+
+    showInTicker.set_cell_data_func(renderer, function (tree, cell, model, iter) {
+      cell.markup = model.get_value(iter, 2).toString()
     })
   }
 
@@ -298,37 +381,28 @@ var PrefsWidget = GObject.registerClass({
    * this recreates the TreeView (Symbol Table)
    */
   refreshUI () {
-    this.mainWidget = this.Window.get_object('prefs-notebook')
+    const stockItems = this.symbolPairs
     this.treeview = this.Window.get_object('tree-treeview')
     this.liststore = this.Window.get_object('tree-liststore')
 
-    this.Window.get_object('tree-toolbutton-remove').sensitive = Boolean(this.symbolPairs.length)
-    this.Window.get_object('tree-toolbutton-edit').sensitive = Boolean(this.symbolPairs.length)
-
-    if (currentSymbolData == this.symbolPairs) {
-      return
-    }
+    this.Window.get_object('tree-toolbutton-remove').sensitive = Boolean(stockItems.length)
+    this.Window.get_object('tree-toolbutton-edit').sensitive = Boolean(stockItems.length)
 
     if (this.liststore) {
       this.liststore.clear()
     }
 
-    if (this.symbolPairs.length > 0) {
-      const symbolPairList = String(this.symbolPairs).split('-&&-')
-
+    if (stockItems.length > 0) {
       let current = this.liststore.get_iter_first()
 
-      symbolPairList.forEach(symbolPair => {
-
-        symbolPair = symbolPair.split('-§§-')
+      stockItems.forEach(stockItem => {
 
         current = this.liststore.append()
-        this.liststore.set_value(current, 0, symbolPair[0])
-        this.liststore.set_value(current, 1, symbolPair[1])
+        this.liststore.set_value(current, 0, stockItem.name)
+        this.liststore.set_value(current, 1, stockItem.symbol)
+        this.liststore.set_value(current, 2, stockItem.showInTicker)
       })
     }
-
-    currentSymbolData = this.symbolPairs
   }
 
   /**
@@ -351,17 +425,15 @@ var PrefsWidget = GObject.registerClass({
 
     // check if we have data (normally we should otherwise it could not be selected...)
     const selectionIndex = parseInt(selection[0][0].to_string())
-    let values = this.splittedSymbolPairs
-    const selectedValue = values[selectionIndex]
+    const selectedStock = this.symbolPairs[selectionIndex]
 
-    if (!selectedValue) {
+    if (!selectedStock) {
       return
     }
 
-    let symbolValues = selectedValue.split('-§§-')
-
-    this.editName.set_text(symbolValues[0])
-    this.editSymbol.set_text(symbolValues[1])
+    this.editName.set_text(selectedStock.name)
+    this.editSymbol.set_text(selectedStock.symbol)
+    this.editShowInTicker.set_state(selectedStock.showInTicker)
 
     this.editWidget.show_all()
   }
@@ -370,11 +442,18 @@ var PrefsWidget = GObject.registerClass({
    * Save new symbol
    */
   saveSymbol () {
-    let name = this.newName.get_text()
-    let symbol = this.newSymbol.get_text().replace(/ /g, '')
+    const name = this.newName.get_text()
+    const symbol = this.newSymbol.get_text().replace(/ /g, '')
+    const showInTicker = this.newShowInTicker.get_state()
+
+    const newItem = {
+      name,
+      symbol,
+      showInTicker
+    }
 
     // append new item and write it to config
-    this.symbolPairs = this.symbolPairs ? this.symbolPairs + '-&&-' + name + '-§§-' + symbol : name + '-§§-' + symbol
+    this.symbolPairs = [...this.symbolPairs, newItem]
 
     this.createWidget.hide()
   }
@@ -390,18 +469,22 @@ var PrefsWidget = GObject.registerClass({
       return
     }
 
-    // check if we have data (normally we should otherwise it could not be selected...)
+    const stockItems = this.symbolPairs
     const selectionIndex = parseInt(selection[0][0].to_string())
-    let values = this.splittedSymbolPairs
-    const selectedValue = values[selectionIndex]
+    const selectedStock = stockItems[selectionIndex]
 
-    if (!selectedValue) {
+    if (!selectedStock) {
       return
     }
 
-    // set new data and write it to config
-    values[selectionIndex] = this.editName.get_text() + '-§§-' + this.editSymbol.get_text().replace(/ /g, '')
-    this.symbolPairs = values.join('-&&-')
+    const newStockItem = {
+      name: this.editName.get_text().trim(),
+      symbol: this.editSymbol.get_text().trim(),
+      showInTicker: this.editShowInTicker.get_state()
+    }
+
+    stockItems[selectionIndex] = newStockItem
+    this.symbolPairs = stockItems
 
     this.editWidget.hide()
   }
@@ -417,70 +500,23 @@ var PrefsWidget = GObject.registerClass({
       return
     }
 
-    // check if we have data (normally we should otherwise it could not be selected...)
+    const stockItems = this.symbolPairs
     const selectionIndex = parseInt(selection[0][0].to_string())
-    let values = this.splittedSymbolPairs
-    const selectedValue = values[selectionIndex]
+    const selectedStock = stockItems[selectionIndex]
 
-    if (!selectedValue) {
+    if (!selectedStock) {
       return
     }
 
-    // show confirm dialog
-    let [name, symbol] = selectedValue.split('-§§-')
+    stockItems.splice(selectionIndex, 1)
 
-    let textDialog = _('Remove %s ?').format(name)
-    let dialog = new Gtk.Dialog({
-      title: ''
-    })
-
-    let label = new Gtk.Label({
-      label: textDialog
-    })
-
-    label.margin_bottom = 12
-
-    dialog.set_border_width(12)
-    dialog.set_modal(1)
-    dialog.set_resizable(0)
-    //dialog.set_transient_for(***** Need parent Window *****);
-
-    dialog.add_button(Gtk.STOCK_NO, 0)
-    let d = dialog.add_button(Gtk.STOCK_YES, 1)
-
-    d.set_can_default(true)
-    dialog.set_default(d)
-
-    let dialog_area = dialog.get_content_area()
-    dialog_area.pack_start(label, 0, 0, 0)
-    dialog.connect('response', (w, response_id) => {
-      if (response_id) {
-        var currentQuotes = {}
-
-        try {
-          currentQuotes = JSON.parse(this.current_quotes || '{}')
-        } catch (e) {
-          log('Could not parse quotes from settings')
-        }
-
-        delete currentQuotes[symbol]
-
-        this.current_quotes = JSON.stringify(currentQuotes)
-
-        // remove entry and write to config
-        values.splice(selectionIndex, 1)
-        this.symbolPairs = values.join('-&&-')
-      }
-      dialog.hide()
-      return 0
-    })
-
-    dialog.show_all()
+    this.symbolPairs = stockItems
   }
 })
 
-function init () {
-  Convenience.initTranslations('stocks@infinicode.de')
+// this is called when settings has been opened
+var init = () => {
+  initTranslations(Settings.SETTINGS_SCHEMA_DOMAIN)
 }
 
 function buildPrefsWidget () {
