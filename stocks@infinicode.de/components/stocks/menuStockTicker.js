bac5f3a36c1691a4038ce73937a4e62ec8ef323c
Florijan Hamzic <fh@infinicode.de>
2021-04-01 16:34:08 +0200

    show multiple ticker items, add ticker item variation

--- stocks@infinicode.de/components/stocks/menuStockTicker.js
+++ stocks@infinicode.de/components/stocks/menuStockTicker.js
@@ -6,8 +6,17 @@ const ExtensionUtils = imports.misc.extensionUtils
 const Me = ExtensionUtils.getCurrentExtension()
 
 const { setTimeout, clearTimeout } = Me.imports.helpers.components
-const { roundOrDefault, getStockColorStyleClass } = Me.imports.helpers.data
-const { Settings, STOCKS_SYMBOL_PAIRS, STOCKS_TICKER_INTERVAL, STOCKS_SHOW_OFF_MARKET_TICKER_PRICES } = Me.imports.helpers.settings
+const { roundOrDefault, getStockColorStyleClass, isNullOrEmpty } = Me.imports.helpers.data
+
+const {
+  Settings,
+  STOCKS_SYMBOL_PAIRS, STOCKS_TICKER_INTERVAL,
+  STOCKS_SHOW_OFF_MARKET_TICKER_PRICES,
+  STOCKS_TICKER_DISPLAY_VARIATION,
+  STOCKS_TICKER_STOCK_AMOUNT,
+  STOCKS_USE_PROVIDER_INSTRUMENT_NAMES
+} = Me.imports.helpers.settings
+
 const { Translations } = Me.imports.helpers.translations
 
 const { MARKET_STATES } = Me.imports.services.meta.generic
@@ -16,8 +25,17 @@ const FinanceService = Me.imports.services.financeService
 const SETTING_KEYS_TO_REFRESH = [
   STOCKS_SYMBOL_PAIRS,
   STOCKS_TICKER_INTERVAL,
-  STOCKS_SHOW_OFF_MARKET_TICKER_PRICES
+  STOCKS_SHOW_OFF_MARKET_TICKER_PRICES,
+  STOCKS_TICKER_DISPLAY_VARIATION,
+  STOCKS_TICKER_STOCK_AMOUNT,
+  STOCKS_USE_PROVIDER_INSTRUMENT_NAMES
 ]
+
+const TICKER_ITEM_VARIATION = {
+  COMPACT: 0,
+  REGULAR: 1,
+  TREMENDOUS: 2
+}
 
 var MenuStockTicker = GObject.registerClass({
   GTypeName: 'StockExtension_MenuStockTicker'
@@ -25,7 +43,6 @@ var MenuStockTicker = GObject.registerClass({
   _init () {
     super._init({
       style_class: 'menu-stock-ticker',
-      x_expand: true,
       y_align: Clutter.ActorAlign.CENTER,
       reactive: true
     })
@@ -52,72 +69,88 @@ var MenuStockTicker = GObject.registerClass({
 
   async _sync () {
     const tickerEnabledItems = Settings.symbol_pairs.filter(item => item.showInTicker)
-    let stockItem = tickerEnabledItems[this._visibleStockIndex]
+    const tickerBatch = this._getBatch(tickerEnabledItems, this._visibleStockIndex, Settings.ticker_stock_amount)
 
-    if (!stockItem) {
-      this._visibleStockIndex = 0
-      stockItem = tickerEnabledItems[0]
-    }
-
-    if (!stockItem) {
+    if (isNullOrEmpty(tickerBatch)) {
       this._showInfoMessage(Translations.EMPTY_TICKER_TEXT)
       return
     }
 
     const showLoadingInfoTimeoutId = setTimeout(this._showInfoMessage.bind(this), 500)
 
-    const quoteSummary = await FinanceService.getQuoteSummary({
+    const quoteSummaries = await Promise.all(tickerBatch.map(stockItem => FinanceService.getQuoteSummary({
       ...stockItem,
       fallbackName: stockItem.name
-    })
+    })))
 
     clearTimeout(showLoadingInfoTimeoutId)
 
-    this._createMenuTicker({ quoteSummary })
+    this._createMenuTicker({ quoteSummaries })
   }
 
-  _createMenuTicker ({ quoteSummary }) {
+  _createMenuTicker ({ quoteSummaries }) {
     this.destroy_all_children()
 
-    const stockInfoDetails = {
-      name: quoteSummary.FullName,
-      currencySymbol: quoteSummary.CurrencySymbol,
-      price: quoteSummary.Close,
-      change: quoteSummary.Change,
-      changePercent: quoteSummary.ChangePercent,
-      isOffMarket: false
-    }
+    const tickerItemCreationFn = this._getTickerItemCreationFunction()
 
-    if (Settings.show_ticker_off_market_prices) {
-      if (quoteSummary.MarketState === MARKET_STATES.PRE) {
-        stockInfoDetails.price = quoteSummary.PreMarketPrice
-        stockInfoDetails.change = quoteSummary.PreMarketChange
-        stockInfoDetails.changePercent = quoteSummary.PreMarketChangePercent
-        stockInfoDetails.isOffMarket = true
+    quoteSummaries.forEach((quoteSummary, index) => {
+      const stockTickerItemBox = tickerItemCreationFn.call(this, quoteSummary)
+      this.add_child(stockTickerItemBox)
+
+      if (index + 1 !== quoteSummaries.length) {
+        const separatorBin = new St.Bin({
+          style_class: 'separator-bin',
+          y_align: Clutter.ActorAlign.CENTER,
+          child: new St.Label({
+            style_class: 'separator',
+            text: '|'
+          })
+        })
+
+        this.add_child(separatorBin)
       }
-
-      if (quoteSummary.MarketState === MARKET_STATES.POST) {
-        stockInfoDetails.price = quoteSummary.PostMarketPrice
-        stockInfoDetails.change = quoteSummary.PostMarketChange
-        stockInfoDetails.changePercent = quoteSummary.PostMarketChangePercent
-        stockInfoDetails.isOffMarket = true
-      }
-    }
-
-    const stockInfoBox = this._createStockInfoBox(stockInfoDetails)
-
-    this.add_child(stockInfoBox)
+    })
   }
 
-  _createStockInfoBox ({ name, currencySymbol, price, change, changePercent, isOffMarket }) {
+  _createCompactTickerItemBox (quoteSummary) {
+    let { name, currencySymbol, price, change, isOffMarket } = this._generateTickerInformation(quoteSummary)
     const quoteColorStyleClass = getStockColorStyleClass(change)
 
     currencySymbol = currencySymbol || ''
 
     const stockInfoBox = new St.BoxLayout({
-      style_class: 'stock-info-box',
-      vertical: true,
-      y_align: Clutter.ActorAlign.END
+      style_class: 'stock-info-box compact',
+      vertical: false,
+      y_align: Clutter.ActorAlign.CENTER
+    })
+
+    const stockNameLabel = new St.Label({
+      style_class: 'ticker-stock-name-label',
+      text: name || Translations.UNKNOWN
+    })
+
+    stockInfoBox.add_child(stockNameLabel)
+
+    const stockQuoteLabel = new St.Label({
+      style_class: `ticker-stock-quote-label fwb ${quoteColorStyleClass}`,
+      text: `${roundOrDefault(price)}${currencySymbol}${isOffMarket ? '*' : ''}`
+    })
+
+    stockInfoBox.add_child(stockQuoteLabel)
+
+    return stockInfoBox
+  }
+
+  _createTremendousTickerItemBox (quoteSummary, regular) {
+    let { name, currencySymbol, price, change, changePercent, isOffMarket } = this._generateTickerInformation(quoteSummary)
+    const quoteColorStyleClass = getStockColorStyleClass(change)
+
+    currencySymbol = currencySymbol || ''
+
+    const stockInfoBox = new St.BoxLayout({
+      style_class: `stock-info-box ${regular ? 'regular' : 'tremendous'}`,
+      vertical: !regular,
+      y_align: Clutter.ActorAlign.CENTER
     })
 
     const stockNameLabel = new St.Label({
@@ -208,5 +241,62 @@ var MenuStockTicker = GObject.registerClass({
     if (this._settingsChangedId) {
       Settings.disconnect(this._settingsChangedId)
     }
+  }
+
+  _getTickerItemCreationFunction () {
+    switch (Settings.ticker_display_variation) {
+      case TICKER_ITEM_VARIATION.COMPACT:
+        return this._createCompactTickerItemBox
+
+      default:
+      case TICKER_ITEM_VARIATION.REGULAR:
+        return quoteSummary => this._createTremendousTickerItemBox(quoteSummary, true)
+
+      case TICKER_ITEM_VARIATION.TREMENDOUS:
+        return this._createTremendousTickerItemBox
+    }
+  }
+
+  _generateTickerInformation (quoteSummary) {
+    const stockInfoDetails = {
+      name: quoteSummary.FullName,
+      currencySymbol: quoteSummary.CurrencySymbol,
+      price: quoteSummary.Close,
+      change: quoteSummary.Change,
+      changePercent: quoteSummary.ChangePercent,
+      isOffMarket: false
+    }
+
+    if (Settings.show_ticker_off_market_prices) {
+      if (quoteSummary.MarketState === MARKET_STATES.PRE) {
+        stockInfoDetails.price = quoteSummary.PreMarketPrice
+        stockInfoDetails.change = quoteSummary.PreMarketChange
+        stockInfoDetails.changePercent = quoteSummary.PreMarketChangePercent
+        stockInfoDetails.isOffMarket = true
+      }
+
+      if (quoteSummary.MarketState === MARKET_STATES.POST) {
+        stockInfoDetails.price = quoteSummary.PostMarketPrice
+        stockInfoDetails.change = quoteSummary.PostMarketChange
+        stockInfoDetails.changePercent = quoteSummary.PostMarketChangePercent
+        stockInfoDetails.isOffMarket = true
+      }
+    }
+
+    return stockInfoDetails
+  }
+
+  _getBatch (items, index, amount) {
+    const start = index * amount
+    const end = start + amount
+
+    const batch = items.slice(start, end)
+
+    if (isNullOrEmpty(batch)) {
+      this._visibleStockIndex = 0
+      return items.slice(0, amount)
+    }
+
+    return batch
   }
 })
--- stocks@infinicode.de/helpers/data.js
+++ stocks@infinicode.de/helpers/data.js
@@ -1,3 +1,4 @@
+const ByteArray = imports.byteArray;
 const { GLib } = imports.gi
 
 let CACHE = {}
@@ -11,7 +12,7 @@ var closest = (array, target) => array.reduce((prev, curr) => Math.abs(curr - ta
 
 var decodeBase64JsonOrDefault = (encodedJson, defaultValue) => {
   try {
-    const value = JSON.parse(GLib.base64_decode(encodedJson))
+    const value = JSON.parse(ByteArray.toString(GLib.base64_decode(encodedJson)))
 
     if (!value) {
       return defaultValue
