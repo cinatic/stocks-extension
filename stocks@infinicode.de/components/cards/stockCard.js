583daf7b22ce5975586bab13c3654830ec2b2ded
Florijan Hamzic <fh@infinicode.de>
2021-04-17 22:16:58 +0200

    remove 'hover' flag for card component

--- stocks@infinicode.de/components/cards/stockCard.js
+++ stocks@infinicode.de/components/cards/stockCard.js
@@ -14,8 +14,7 @@ var StockCard = GObject.registerClass({
     super._init({
       style_class: 'card message stock-card',
       can_focus: true,
-      x_expand: true,
-      hover: true
+      x_expand: true
     })
 
     this.cardItem = quoteSummary
