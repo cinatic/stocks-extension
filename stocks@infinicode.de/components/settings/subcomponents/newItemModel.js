10999c2ef2a365918344da4efa511f4992538d8f
Florijan Hamzic <fh@infinicode.de>
2022-04-15 10:42:45 +0200

    add components for new settings experience (:D)

---
+++ stocks@infinicode.de/components/settings/subcomponents/newItemModel.js
@@ -0,0 +1,19 @@
+const { Gio, GObject } = imports.gi
+
+var NewItemModel = GObject.registerClass(class NewItemModelClass extends GObject.Object {
+  static [GObject.interfaces] = [Gio.ListModel]
+
+  #item = new GObject.Object()
+
+  vfunc_get_item_type () {
+    return GObject.Object
+  }
+
+  vfunc_get_n_items () {
+    return 1
+  }
+
+  vfunc_get_item (_pos) {
+    return this.#item
+  }
+})
