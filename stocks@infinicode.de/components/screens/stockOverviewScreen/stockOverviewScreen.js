0e7b9d7ff34fc53dfdd6952aff8879ca8166741e
Florijan Hamzic <fh@infinicode.de>
2023-06-03 15:42:19 +0200

    integrate new portfolio structure into overview

--- stocks@infinicode.de/components/screens/stockOverviewScreen/stockOverviewScreen.js
+++ stocks@infinicode.de/components/screens/stockOverviewScreen/stockOverviewScreen.js
@@ -5,15 +5,18 @@ const Mainloop = imports.mainloop
 const ExtensionUtils = imports.misc.extensionUtils
 const Me = ExtensionUtils.getCurrentExtension()
 
+const { ButtonGroup } = Me.imports.components.buttons.buttonGroup
 const { FlatList } = Me.imports.components.flatList.flatList
 const { StockCard } = Me.imports.components.cards.stockCard
 const { SearchBar } = Me.imports.components.searchBar.searchBar
 const { setTimeout, clearTimeout } = Me.imports.helpers.components
-const { removeCache } = Me.imports.helpers.data
+const { isNullOrEmpty, removeCache } = Me.imports.helpers.data
 
 const {
   SettingsHandler,
+  STOCKS_PORTFOLIOS,
   STOCKS_SYMBOL_PAIRS,
+  STOCKS_SELECTED_PORTFOLIO,
   STOCKS_USE_PROVIDER_INSTRUMENT_NAMES
 } = Me.imports.helpers.settings
 
@@ -23,6 +26,8 @@ const FinanceService = Me.imports.services.financeService
 
 const SETTING_KEYS_TO_REFRESH = [
   STOCKS_SYMBOL_PAIRS,
+  STOCKS_SELECTED_PORTFOLIO,
+  STOCKS_PORTFOLIOS,
   STOCKS_USE_PROVIDER_INSTRUMENT_NAMES
 ]
 
@@ -44,9 +49,11 @@ var StockOverviewScreen = GObject.registerClass({
     this._settings = new SettingsHandler()
 
     this._searchBar = new SearchBar({ mainEventHandler: this._mainEventHandler })
-    this._list = new FlatList()
+    this._portfolioGroup = new ButtonGroup({ buttons: [], y_expand: false })
+    this._list = new FlatList({ id: 'stock_overview', persistScrollPosition: true })
 
     this.add_child(this._searchBar)
+    this.add_child(this._portfolioGroup)
     this.add_child(this._list)
 
     this.connect('destroy', this._onDestroy.bind(this))
@@ -54,6 +61,7 @@ var StockOverviewScreen = GObject.registerClass({
     this._searchBar.connect('refresh', () => {
       removeCache('summary_')
       this._loadData()
+      this._createPortfolioButtonGroup()
     })
 
     this._searchBar.connect('text-change', (sender, searchText) => this._filter_results(searchText))
@@ -61,6 +69,7 @@ var StockOverviewScreen = GObject.registerClass({
     this._settingsChangedId = this._settings.connect('changed', (value, key) => {
       if (SETTING_KEYS_TO_REFRESH.includes(key)) {
         this._loadData()
+        this._createPortfolioButtonGroup()
       }
     })
 
@@ -70,6 +79,8 @@ var StockOverviewScreen = GObject.registerClass({
         item: item.cardItem
       }
     }))
+
+    this._createPortfolioButtonGroup()
 
     this._loadData()
 
@@ -93,6 +104,33 @@ var StockOverviewScreen = GObject.registerClass({
     })
   }
 
+  async _createPortfolioButtonGroup () {
+    const portfolios = this._settings.portfolios
+
+    if (!portfolios.find(item => item.id == this._settings.selected_portfolio)) {
+      this._settings.selected_portfolio = this._settings.portfolios[0].id
+    }
+
+    let buttons = []
+
+    if (portfolios && portfolios.length > 1) {
+      buttons = portfolios.map(item => ({
+        label: item.name,
+        value: item.id,
+        selected: this._settings.selected_portfolio == item.id
+      }))
+    }
+
+    const newButtonGroup = new ButtonGroup({ buttons, y_expand: false })
+    newButtonGroup.connect('clicked', (_, stButton) => {
+      this._settings.selected_portfolio = stButton.buttonData.value
+    })
+
+    this.replace_child(this._portfolioGroup, newButtonGroup)
+
+    this._portfolioGroup = newButtonGroup
+  }
+
   _registerTimeout () {
     this._autoRefreshTimeoutId = Mainloop.timeout_add_seconds(this._settings.ticker_interval || 10, () => {
       this._loadData()
@@ -106,7 +144,12 @@ var StockOverviewScreen = GObject.registerClass({
       return
     }
 
-    if (!this._settings.symbol_pairs) {
+    const portfolios = this._settings.portfolios
+    const selectedOrFirstPortfolio = portfolios.find(item => item.id == this._settings.selected_portfolio) || portfolios[0]
+
+    const symbols = selectedOrFirstPortfolio.symbols
+
+    if (isNullOrEmpty(symbols)) {
       this._list.show_error_info(Translations.NO_SYMBOLS_CONFIGURED_ERROR)
       return
     }
@@ -116,7 +159,7 @@ var StockOverviewScreen = GObject.registerClass({
     this._showLoadingInfoTimeoutId = setTimeout(() => this._list.show_loading_info(), 500)
 
     const quoteSummaries = await Promise.all(
-        this._settings.symbol_pairs.map(symbolData => FinanceService.getQuoteSummary({
+        symbols.map(symbolData => FinanceService.getQuoteSummary({
           ...symbolData,
           fallbackName: symbolData.name
         }))
@@ -131,6 +174,15 @@ var StockOverviewScreen = GObject.registerClass({
     })
 
     this._filter_results(this._searchBar.search_text())
+
+    // const savedScrollPosition = await cacheOrDefault('scroll_stock_overview', () => this._list.vscroll.adjustment.value, 365 * 24 * 60 * 60 * 1000)
+    //
+    // setTimeout(() => {
+    //   if (this._list.vscroll.adjustment.value !== savedScrollPosition) {
+    //     // could not figure out which event might be appropriate to set the position properly, so we work with this
+    //     this._list.vscroll.adjustment.value = savedScrollPosition
+    //   }
+    // }, 50)
 
     this._isRendering = false
   }
